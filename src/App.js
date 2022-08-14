7b3a96f6a56e86b4d3c6e8c5baf292c6518a2b11
jhhg04 <jhhg04@gmail.com>
2022-08-14 11:23:33 -0500

    add

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
-import { useSelector } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TaskForm from './components/TaskForm';
 import TasksList from './components/TasksList';
 
 function App() {
-  const tasksState = useSelector((state) => state.tasks);
-  console.log(tasksState);
   return (
-    <div className='App'>
-      <h1>Learn React</h1>
-      <TaskForm />
-      <TasksList />
+    <div className='bg-zinc-900 h-screen text-white'>
+      <div className='flex items-center justify-center h-full '>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<TasksList />} />
+            <Route path='/create-task' element={<TaskForm />} />
+            <Route path='/edit-task/:id' element={<TaskForm />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
